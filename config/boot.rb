02fb5974b5daac4c21997d0fa866f5b2a974d7ff
Raul Vinicius <raul.portifolio@gmail.com>
2014-03-09 19:10:46 -0300

    rails running wild

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+$LOAD_PATH.unshift('C:/RailsInstaller/Ruby1.9.3/bin')
