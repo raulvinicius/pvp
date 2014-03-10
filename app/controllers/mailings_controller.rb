39d3403a9393def24f94625250626f3a8233aea1
Raul Vinicius <raul.portifolio@gmail.com>
2014-03-10 04:47:33 -0300

    ajustes finais no mailing do teaser

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class MailingsController < ApplicationController
   before_action :set_mailing, only: [:show, :edit, :update, :destroy]
 
@@ -13,13 +14,24 @@ class MailingsController < ApplicationController
 
     respond_to do |format|
       if @mailing.save
-        format.html { redirect_to @mailing, notice: 'Mailing was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Você será avisado assim que as engrenagens começarem e rodar' }
         format.json { render action: 'show', status: :created, location: @mailing }
       else
         format.html { render action: 'new' }
         format.json { render json: @mailing.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_mailing
+    @mailing = Mailing.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def mailing_params
+    params.require(:mailing).permit(:name, :email, :allow)
   end
 
 end
